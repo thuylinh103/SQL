cc0a5034c55eca0ea5e240b0e869ebdca3b03093
thuylinh103 <linhtinh837@gmail.com>
2025-01-05 21:08:13 +0700

    Add files via upload

--- QLBH.sql
+++ QLBH.sql
@@ -1,4 +1,4 @@
-USE [NewQLCHTL]
+﻿USE [NewQLCHTL]
 GO
 SET ANSI_NULLS ON
 GO
@@ -173,4 +173,61 @@ GO
 ALTER TABLE [dbo].[tblHDBan] CHECK CONSTRAINT [FK_tblHDBan_tblNhanVien]
 GO
 
+-- Thêm dữ liệu mẫu vào bảng NhaCungCap
+INSERT INTO [dbo].[NhaCungCap] VALUES
+('NCC01', 'Công ty A', '0123456789', 'Hà Nội'),
+('NCC02', 'Công ty B', '0987654321', 'Hồ Chí Minh');
+
+-- Thêm dữ liệu mẫu vào bảng tblChatLieu
+INSERT INTO [dbo].[tblChatLieu] VALUES
+('CL01', 'Cotton'),
+('CL02', 'Polyester');
+
+-- Thêm dữ liệu mẫu vào bảng tblChiTietHDBan
+INSERT INTO [dbo].[tblChiTietHDBan] VALUES
+('HDB01', 'H01', 2, 50000, 0, 100000),
+('HDB02', 'H02', 3, 60000, 10, 162000);
+
+-- Thêm dữ liệu mẫu vào bảng tblHang
+INSERT INTO [dbo].[tblHang] VALUES
+('H01', 'Áo thun', 'CL01', 50, 40000, 50000, 'Size M'),
+('H02', 'Quần jeans', 'CL02', 30, 50000, 60000, 'Size L');
+
+-- Thêm dữ liệu mẫu vào bảng tblHDBan
+INSERT INTO [dbo].[tblHDBan] VALUES
+('HDB01', 'NV01', '2023-01-01', 'KH01', 100000),
+('HDB02', 'NV02', '2023-01-02', 'KH02', 162000);
+
+-- Thêm dữ liệu mẫu vào bảng tblKhach
+INSERT INTO [dbo].[tblKhach] VALUES
+('KH01', 'Nguyễn Văn A', 'Hà Nội', '0901234567'),
+('KH02', 'Trần Thị B', 'Hồ Chí Minh', '0912345678');
+
+-- Thêm dữ liệu mẫu vào bảng tblNhanVien
+INSERT INTO [dbo].[tblNhanVien] VALUES
+('NV01', 'Lê Văn C', 'Nam', 'Hà Nội', '0981111111', '1990-01-01'),
+('NV02', 'Phạm Thị D', 'Nữ', 'Hồ Chí Minh', '0982222222', '1995-02-01');
+
+-- Thêm dữ liệu mẫu cho bảng tblQltkAdmin
+INSERT INTO [dbo].[tblQltkAdmin] ([TenDangNhap], [MatKhau]) VALUES 
+('admin1', 'password123'),
+('admin2', 'securepass456'),
+('admin3', 'adminpass789');
+
+-- Thêm dữ liệu mẫu cho bảng tblQltkNhanVien
+INSERT INTO [dbo].[tblQltkNhanVien] ([TenDangNhap], [MatKhau]) VALUES 
+('nv001', 'nvpass123'),
+('nv002', 'nvpass456'),
+('nv003', 'nvpass789'),
+('nv004', 'nvpass101'),
+('nv005', 'nvpass202');
+
+select*from NhaCungCap
 select*from tblChatLieu
+select*from tblChiTietHDBan
+select*from tblHang
+select*from tblHDBan
+select*from tblKhach
+select*from tblNhanVien
+select*from tblQltkAdmin
+select*from tblQltkNhanVien
